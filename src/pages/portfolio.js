664f125a8b437c6800ce6d113e3a203110cb08bb
Frédéric Lang <f.lang@hotmail.fr>
2020-07-11 12:09:49 +0200

    new falcon picture

---
+++ src/pages/portfolio.js
@@ -0,0 +1,18 @@
+
+
+export default () => (
+    <div className="container">
+      <main>
+        <h1 className="title">
+          Portfolio
+        </h1>
+  
+        <p className="description">
+          Photeus Website
+        </p>
+  
+      </main>
+    </div>
+  )
+  
+  
