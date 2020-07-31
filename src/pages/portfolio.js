11305d260c70c693776d6137ef17164a3b447d1a
Frédéric Lang <f.lang@hotmail.fr>
2020-07-31 18:27:25 +0200

    new portfolio with link to jacar

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -17,18 +17,20 @@ export default () => (
         <section>
         <div className="container">
         <div className="tile is-ancestor">
-          <div className="tile is-child is-4">
-          <div className="box">
+        <div className="tile is-parent">
+          <a href="https://jacar-frontend.vercel.app/">
+          <div className="tile is-child is-8 box">
             <p className="title">Jacar</p>
             <p>Jacar is a social media platform narrowing tourism. Is allows you to share photos of your trips, and get inspiration from others.</p>
           </div>
-          </div>
+          </a>
           <div className="tile is-child is-4">
           <div className="box">
-            <p className="title">Photeus</p>
-            <p>Photeus is a portfolio website. Its displays all projects I am currently working on.</p>
+            <p className="title">Others Projects to come ...</p>
+            <p>I am currently working mainly on Jacar. Come later to see if I added some new projects !</p>
           </div>
           </div>
+        </div>
         </div>
         </div>
         </section>
