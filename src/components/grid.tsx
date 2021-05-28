d34fad3fe626fc16764df1f8e67665ccfc9d55b4
Frédéric Lang <f.lang@hotmail.fr>
2021-05-28 23:13:06 +0200

    update values

---
+++ src/components/grid.tsx
@@ -0,0 +1,30 @@
+
+
+const Tricks = () => (
+
+    <section className="py-20 bg-white">
+        <div className="grid grid-cols-3 gap-4">
+            <div className="border col-span-1">
+              1
+            </div>
+            <div className="border col-span-1">
+              2
+            </div>
+            <div className="border col-span-1">
+              3
+            </div>
+            <div className="border col-span-1">
+              4
+            </div>
+            <div className="border col-span-1">
+              5
+            </div>
+            <div className="border col-span-1">
+              6
+            </div>
+        </div>
+    </section>
+
+)
+
+export default Tricks;
--- src/pages/kitchensink.tsx
+++ src/pages/kitchensink.tsx
@@ -1,24 +1,15 @@
 import Technos from '../components/Technos/Technos';
 import Content1 from '../components/content1';
 import Hero1 from '../components/Hero1'
-import Hero2 from '../components/Hero2'
 import Hero3 from '../components/Hero3'
+import Grid from '../components/grid'
 import Technologies from '../components/technologies';
 import CallToAction from '../components/CallToAction';
 
 const Index = () => (
-    <>
-    <Hero1 />
-    <Content1 />
-    <div className="w-5/6 mx-auto my-16 border"/>
-    <div id="technologies" >
-    <Technologies />
-    </div>
-    <Technos />
-    <Hero2 />
+    <div className="m-8">
     <Hero3 />
-    <CallToAction />
-  </>
+    </div>
 );
 
 export default Index;
